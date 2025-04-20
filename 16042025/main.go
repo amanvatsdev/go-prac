6eee4fe4b5273e15f769d7be34b66a883118a04c
Aman Vats <amanvatsstudent@gmail.com>
2025-04-21 05:17:52 +0530

    sdsdfsdfsdsd

---
+++ 16042025/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func PerformTask(x,y int,callback func(int,int)int){
+	fmt.Println(callback(x,y))
+}
+func main() {
+	Addition:=func (x ,y int)int  {
+	return x+y
+	}
+	Subraction:=func(x,y int)int{
+		return x-y
+	}
+	Multiplication:=func(x,y int)int{
+		return x*y
+	}
+	Division:=func(x,y int)int{
+		return x/y
+	}
+	fmt.Println("plz give me two number one by one")
+	var N1 int
+	fmt.Scanln(&N1)
+	var N2 int
+	fmt.Scanln(&N2)
+	fmt.Println("Now plz tell me the task you want to perform plz select an option A,B,C,D")
+	fmt.Println(`
+	A=Additon,
+	B=Subtraction
+	C=Multiplication
+	D=Division`)
+	var task string
+	fmt.Scanln(&task)
+	
+	switch  {
+	case task=="A"||task=="a":
+		PerformTask(N1,N2,Addition)
+	case task=="B"||task=="b":
+		PerformTask(N1,N2,Subraction)
+	case task=="C"||task=="c":
+		PerformTask(N1,N2,Multiplication)
+	case task=="D"||task=="d":
+		PerformTask(N1,N2,Division)
+	default:
+		fmt.Println("Choose the valid option")
+	}
+	
+
+	// a, b := 1, 2
+	// fmt.Println(a,b)
+	// fmt.Println(Addition(2,2))
+	// PerformTask(34,34,Addition)
+
+}
