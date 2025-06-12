3690c82c1b15c4b2390a992f07d4fc5f6a186f09
Aman Vats <amanvatsstudent@gmail.com>
2025-06-12 13:46:58 +0530

    updates of 06062025

---
+++ 06062025/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	AnimalsSound "github.com/amanvatsdev/Puppy/Animals_Sound"
+)
+
+func main() {
+	A:=AnimalsSound.DogBark
+	fmt.Println(A)
+	fmt.Println(AnimalsSound.Cat)
+	// fmt.Println()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module merger
 
 go 1.24.1
+
+require github.com/amanvatsdev/Puppy v0.0.0-20250606064457-f1e44373399b
